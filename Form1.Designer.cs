ab7113aaa75b5a24dac4fa4ad0c69f794a7875c4
Huiiyi <Huiiyi@users.noreply.github.com>
2018-08-27 16:03:07 +0800

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WallPaperSoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(979, 564);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://area.sinaapp.com/bingImg/", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 452);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "设置壁纸";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "设置目录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 545);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(979, 19);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 564);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bing每日壁纸";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WallPaperSoft
+{
+    public partial class Form1 : Form
+    {
+        long totalBytes = 0;
+        long totalDownloadedByte = 0;
+        string path = null;
+        public delegate void download_dg();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.webBrowser1.Document.Body.Style = "zoom:0.25";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "\\" + "SavePath.ini"))
+            {
+                StreamReader sr = new StreamReader(Application.StartupPath + "\\" + "SavePath.ini", Encoding.Default);
+                path = sr.ReadLine();
+                sr.Close();
+            }
+            else
+            {
+                folderBrowserDialog1.ShowDialog();
+                path = folderBrowserDialog1.SelectedPath + "\\";
+                File.WriteAllText(Application.StartupPath + "\\" + "SavePath.ini", folderBrowserDialog1.SelectedPath + "\\");
+            }
+            //创建线程进行下载
+            download_dg dg = new download_dg(DownLoadFile);
+            dg.BeginInvoke(null, null);
+            //Thread newThread = new Thread(DownLoadFile);
+            ////newThread.SetApartmentState(ApartmentState.MTA); 
+            //newThread.SetApartmentState(ApartmentState.STA);//设置这个参数，指示应用程序的COM线程模型 是 单线程单元
+            //newThread.Start();
+        }
+        private void DownLoadFile()
+        {
+            string url = webBrowser1.Url.AbsoluteUri;
+            //正则表达式提取文件名
+            string[] str = url.Split('/');
+            string file = str.Last();
+            //如果没有设置过路径则设置储存文件路径，否则直接读取
+            //if (File.Exists(Application.StartupPath + "\\" + "SavePath.ini"))
+            //{
+            //    StreamReader sr = new StreamReader(Application.StartupPath + "\\" + "SavePath.ini", Encoding.Default);
+            //    path = sr.ReadLine();
+            //    sr.Close();
+            //}
+            //else
+            //{
+            //    folderBrowserDialog1.ShowDialog();
+            //    path = folderBrowserDialog1.SelectedPath + "\\";
+            //    File.WriteAllText(Application.StartupPath + "\\" + "SavePath.ini", folderBrowserDialog1.SelectedPath + "\\");
+            //}
+            string filename = path + file;
+            //指定url 下载文件
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            Stream stream = request.GetResponse().GetResponseStream();
+            //获得文件大小
+            totalBytes = request.GetResponse().ContentLength;
+            //创建写入流
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            byte[] bytes = new byte[1024 * 10];
+            int readCount = 0;
+            while (true)
+            {
+                readCount = stream.Read(bytes, 0, bytes.Length);
+                totalDownloadedByte = readCount + totalDownloadedByte;
+                if (readCount <= 0)
+                    break;
+                fs.Write(bytes, 0, readCount);
+                fs.Flush();
+            }
+            fs.Close();
+            stream.Close();
+            SetWallPaper(filename);
+        }
+        //更换壁纸
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        private void SetWallPaper(string fn)
+        {
+            //使用Split以"."分割路径，取不带后缀的文件路径
+            string[] f = fn.Split('.');
+            string file = f.First();
+            //换壁纸函数需要bmp格式
+            Image image = Image.FromFile(fn);
+            image.Save(file+".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            SystemParametersInfo(20, 0, file + ".bmp", 0x2);
+            Thread.Sleep(2000);
+            File.Delete(file + ".bmp");
+            totalDownloadedByte = 0;
+        }
+
+        //更新进度条进度
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1 != null && totalBytes != 0)
+            {
+                progressBar1.Maximum = (int)(totalBytes);
+            }
+            progressBar1.Value = (int)(totalDownloadedByte);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            //获取当前程序运行目录创建文件 并写入用户选择的路径保存
+            File.WriteAllText(Application.StartupPath + "\\" + "SavePath.ini", folderBrowserDialog1.SelectedPath + "\\");
+        }
+    }
+}
